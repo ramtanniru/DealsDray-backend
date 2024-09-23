dea35c048468e6cb5d7997b878b2a590ba411ac5
ram_tanniru <ramtanniru2@gmail.com>
2024-09-23 14:56:22 +0530

    changed default port

--- app.js
+++ app.js
@@ -211,7 +211,7 @@ app.delete('/employees/:id', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
